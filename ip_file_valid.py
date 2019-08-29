d86943c318caf79d7b596084f19c244f0d602883
Steven <mstevenwilliams@gmail.com>
2019-08-29 17:19:29 -0400

    Finish IP file check module. Add logic to read IP address from txt file.

--- ip_file_valid.py
+++ ip_file_valid.py
@@ -14,3 +14,17 @@ def ip_file_valid():
 	else:
 		print("\n* File () does not exist. Please check and try again.\n".format(ip_file))
 		sys.exit()
+
+	#Open user selected file for reading (IP addresses file)
+	selected_ip_file = open(ip_file, 'r')
+
+	#Starting from the beginning of the file
+	selected_ip_file.seek(0)
+
+	#Reading each item (IP address) in the file
+	ip_list = selected_ip_file.readlines()
+
+	#Closing the file
+	selected_ip_file.close()
+
+	return ip_list
