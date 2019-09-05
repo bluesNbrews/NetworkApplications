4b2c292b9bb4edca896c9362eeebd936ac27d27c
Steven <mstevenwilliams@gmail.com>
2019-09-05 17:43:26 -0400

    Create shell file for ssh connection.

---
+++ ssh_connection.py
@@ -0,0 +1,10 @@
+import paramiko
+import os.path
+import time
+import sys 
+import re
+
+#Checking username/password file
+#Prompting user for input - USERNAME/PASSWORD FILE
+user_file = input("\n# Enter user file path and name (e.g. ~/NetworkApplications/myfile.txt): ")
+
