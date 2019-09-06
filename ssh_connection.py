4748a5e2f09cc23e2db2850a454f681245f9f86c
Steven <mstevenwilliams@gmail.com>
2019-09-06 14:42:31 -0400

    Check validity of username/password file and commands file.

--- ssh_connection.py
+++ ssh_connection.py
@@ -8,3 +8,22 @@ import re
 #Prompting user for input - USERNAME/PASSWORD FILE
 user_file = input("\n# Enter user file path and name (e.g. ~/NetworkApplications/myfile.txt): ")
 
+#Verifying the validity of the USERNAME/PASSWORD file
+if os.path.isfile(user_file) == True:
+	print("\n * Username/password file is valid. \n")
+
+else:
+	print("\n* File {} does not exist. Please check and try again.\n".format(user_file))
+	sys.exit()
+
+#Checking commands file
+#Prompting user for input - COMMANDS FILE
+cmd_file = input("\n# Enter commands file path name (e.g. ~/NetworkApplications/myfile.txt): ")
+
+#Verifying the validity of the COMMANDS FILE
+if os.path.isfile(cmd_file) == True:
+	print("\n* Command file is valid.\n")
+
+else:
+	print("\n* File {} does not exist. Please check and try again.\n".format(cmd_file))
+	sys.exit()
