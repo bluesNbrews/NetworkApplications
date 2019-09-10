b66ab8049725e5956db3570cce73d9d0731722e9
Steven <mstevenwilliams@gmail.com>
2019-09-10 08:33:14 -0400

    Complete module to establish SSH connection.

--- ssh_connection.py
+++ ssh_connection.py
@@ -35,6 +35,76 @@ def ssh_connection(ip):
 	global cmd_file
 
 	#Creating SSH Connection
-	#try:
-	#except:
+	try:
+		#Define SSH parameters
+		selected_user_file = open(user_file, 'r')
 
+		#Starting from the beginning of the file
+		selected_user_file.seek(0)
+
+		#Reading the username from the file
+		username = selected_user_file.readlines()[0].split(',')[0].rstrip("\n")
+
+		#Starting from the beginning of the file
+		selected_user_file.seek(0)
+
+		#Reading the password from the file
+		password = selected_user_file.readlines()[0].split(',')[1].rstrip("\n")
+
+		#Logging into device 
+		session = paramiko.SSHClient()
+
+		#For testing purposes, this allow auto-accepting unknown hohst keys
+		#Not to be used in production. This default would be RejectPolicy
+		session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+		#Connect to the device using username and password
+		session.connect(ip.rstrip("\n"),username = username, password = password)
+
+		#Start an interactive shell using username and password
+		connection = session.invoke_shell()
+
+		#Setting terminal length for entire output - disable pagination
+		connection.send("enable\n")
+		connection.send("terminal length 0\n")
+		time.sleep(1)
+
+		#Entering global configuration mode
+		connection.send("\n")
+		connection.send("configure terminal\n")
+		time.sleep(1)
+
+		#Open user selected file for reading
+		selected_cmd_file = open(cmd_file, 'r')
+
+		#Starting from the beginning of the file
+		selected_cmd_file.seek(0)
+
+		for each_line in selected_cmd_file.readlines():
+			connection.send(each_line + '\n')
+			time.sleep(2)
+
+		#Closing the user file
+		selected_user_file.close()
+
+		#Closing the command file
+		selected_cmd_file.close()
+
+		#Checking command output for IOS syntax errors
+		router_output = connection.recv(65535)
+
+		if re.search(b"% Invalid input", router_output)
+			print("* There was at least one IOS syntax error on device {} :".format(ip))
+
+		else:
+			print("\nDone foe device {}\n".format(ip))
+
+		#Test for reading command output
+		print(str(router_output) + "\n")
+
+		#Closing the connection
+		session.close()
+
+	except paramiko.AuthenticationException:
+		print("* Invalid username or password\n * Please check the username/password file or the device configuration.")
+		print("* Closing program...")
